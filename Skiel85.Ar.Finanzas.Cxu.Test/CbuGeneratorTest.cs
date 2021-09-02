3a2a61b5dfa227489559b8192b1eec29f5a5871f
Ezequiel Alejandro Paez <skiel85@yahoo.com.ar>
2021-09-02 02:27:24 -0300

    Generador de CBUs.

---
+++ Skiel85.Ar.Finanzas.Cxu.Test/CbuGeneratorTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Skiel85.Ar.Finanzas.Cxu.Test
+{
+    public class CbuGeneratorTest
+    {
+        [Fact]
+        public void GeneracionDeCbus()
+        {
+            const int seed = 123;
+            var cbuGenerator = new CbuGenerator(seed);
+            var cbu1 = cbuGenerator.Next();
+            var cbu2 = cbuGenerator.Next();
+            Assert.Equal("9839077100007435455173", cbu1.ToString());
+            Assert.Equal("8117387700000483150175", cbu2.ToString());
+        }
+    }
+}
---
+++ Skiel85.Ar.Finanzas.Cxu/CbuGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Skiel85.Ar.Finanzas.Cxu
+{
+    public class CbuGenerator
+    {
+        private readonly CbuBuilder _builder;
+        private readonly Random _random;
+
+        public CbuGenerator(int seed)
+        {
+            _random = new Random(seed);
+            _builder = new CbuBuilder();
+        }
+
+        public Cbu Next()
+        {
+            var nroEntidad = _random.Next(1, 999);
+            var nroSucursal = _random.Next(1, 9999);
+            var nroCuenta = _random.Next(1, 999999999);
+            return _builder.CrearCbu((uint)nroEntidad, (uint)nroSucursal, 0, (ulong)nroCuenta, 0).CorregirDvs();
+        }
+    }
+}
