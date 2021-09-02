faa93566ef56489688e442990467850da43acb31
Ezequiel Alejandro Paez <skiel85@yahoo.com.ar>
2021-09-01 22:22:27 -0300

    Creación de CBU a partir de string.

---
+++ Skiel85.Ar.Finanzas.Cxu.Test/CreacionCbuTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Skiel85.Ar.Finanzas.Cxu.Test
+{
+    public class CreacionCbuTest
+    {
+        private const string UnCbuValidoStr = "0110012920000091344977";
+
+        [Fact]
+        public void CreacionDesdeString()
+        {
+            var cbu = new Cbu(UnCbuValidoStr);
+            Assert.Equal(UnCbuValidoStr, cbu.ToString());
+        }
+    }
+}
---
+++ Skiel85.Ar.Finanzas.Cxu/Cbu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Skiel85.Ar.Finanzas.Cxu
+{
+    public class Cbu
+    {
+        private readonly string _valor;
+
+        public Cbu(string valor)
+        {
+            _valor = valor;
+        }
+
+        public override string ToString()
+        {
+            return _valor;
+        }
+    }
+}
