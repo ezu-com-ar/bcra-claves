85ef0ca148400dcacccf9849aec483567531619f
Ezequiel Alejandro Paez <skiel85@yahoo.com.ar>
2021-09-01 22:38:21 -0300

    Creación de CBU a partir de bloques y componentes.

--- Skiel85.Ar.Finanzas.Cxu.Test/CreacionCbuTest.cs
+++ Skiel85.Ar.Finanzas.Cxu.Test/CreacionCbuTest.cs
@@ -8,11 +8,32 @@ namespace Skiel85.Ar.Finanzas.Cxu.Test
     public class CreacionCbuTest
     {
         private const string UnCbuValidoStr = "0110012920000091344977";
+        private const string UnCbuValidoBloque1Str = "01100129";
+        private const string UnCbuValidoBloque2Str = "20000091344977";
+        private const string UnCbuValidoNroEntidadStr = "011";
+        private const string UnCbuValidoNroSucursalStr = "0012";
+        private const string UnCbuValidoNroDvBloque1Str = "9";
+        private const string UnCbuValidoNroCuentaStr = "2000009134497";
+        private const string UnCbuValidoNroDvBloque2Str = "7";
 
         [Fact]
         public void CreacionDesdeString()
         {
             var cbu = new Cbu(UnCbuValidoStr);
+            Assert.Equal(UnCbuValidoStr, cbu.ToString());
+        }
+
+        [Fact]
+        public void CreacionDesdeBloques()
+        {
+            var cbu = new Cbu(UnCbuValidoBloque1Str, UnCbuValidoBloque2Str);
+            Assert.Equal(UnCbuValidoStr, cbu.ToString());
+        }
+
+        [Fact]
+        public void CreacionDesdeComponentes()
+        {
+            var cbu = new Cbu(UnCbuValidoNroEntidadStr, UnCbuValidoNroSucursalStr, UnCbuValidoNroDvBloque1Str, UnCbuValidoNroCuentaStr, UnCbuValidoNroDvBloque2Str);
             Assert.Equal(UnCbuValidoStr, cbu.ToString());
         }
     }
--- Skiel85.Ar.Finanzas.Cxu/Cbu.cs
+++ Skiel85.Ar.Finanzas.Cxu/Cbu.cs
@@ -13,6 +13,14 @@ namespace Skiel85.Ar.Finanzas.Cxu
             _valor = valor;
         }
 
+        public Cbu(string bloque1, string bloque2)
+            : this(bloque1 + bloque2)
+        { }
+
+        public Cbu(string nroEntidad, string nroSucursal, string dvBloque1, string nroCuenta, string dvBloque2)
+            : this(nroEntidad + nroSucursal + dvBloque1 + nroCuenta + dvBloque2)
+        { }
+
         public override string ToString()
         {
             return _valor;
