69ada38de6861b9b64d1fe42c54961a61f356f37
Ezequiel Alejandro Paez <skiel85@yahoo.com.ar>
2021-09-03 00:50:44 -0300

    Generación de Cvus.

---
+++ Skiel85.Ar.Finanzas.Cxu.Test/CvuGeneratorTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Skiel85.Ar.Finanzas.Cxu.Test
+{
+    public class CvuGeneratorTest
+    {
+        [Fact]
+        public void GeneracionDeCvus()
+        {
+            const int seed = 123;
+            var cvuGenerator = new CvuGenerator(seed);
+            var cvu1 = cvuGenerator.Next();
+            var cvu2 = cvuGenerator.Next();
+            Assert.Equal("0009844700009078153229", cvu1.ToString());
+            Assert.Equal("0007434400008116416520", cvu2.ToString());
+        }
+    }
+}
---
+++ Skiel85.Ar.Finanzas.Cxu/CvuGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Skiel85.Ar.Finanzas.Cxu
+{
+    public class CvuGenerator
+    {
+        private readonly Random _random;
+
+        public CvuGenerator(int seed)
+        {
+            _random = new Random(seed);
+        }
+
+        public Cvu Next()
+        {
+            var nroPsp = _random.Next(1, 9999).ToString();
+            var idCliente = _random.Next(1, 999999999).ToString();
+            return new Cvu(nroPsp, "0", idCliente, "0").CorregirDvs();
+        }
+    }
+}
