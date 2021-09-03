d7919f79f999d8062d4c8d201846fc657aa06ead
Ezequiel Alejandro Paez <skiel85@yahoo.com.ar>
2021-09-03 00:46:09 -0300

    Creo CxuBuilder para fabricar Cvu y Cbu a partir de un string.

---
+++ Skiel85.Ar.Finanzas.Cxu.Test/CxuBuilderTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Skiel85.Ar.Finanzas.Cxu.Test
+{
+    public class CxuBuilderTest
+    {
+        [Fact]
+        public void CreacionDesdeString()
+        {
+            var builder = new CxuBuilder();
+            var cbu = builder.Crear(CbusDePrueba.UnCbuValidoStr);
+            var cvu = builder.Crear(CvusDePrueba.UnCvuValidoStr);
+            Assert.IsType<Cbu>(cbu);
+            Assert.IsType<Cvu>(cvu);
+            Assert.Equal(CbusDePrueba.UnCbuValidoStr, cbu.ToString());
+            Assert.Equal(CvusDePrueba.UnCvuValidoStr, cvu.ToString());
+        }
+
+        [Fact]
+        public void CreacionDesdeBloques()
+        {
+            var builder = new CxuBuilder();
+            var cbu = builder.Crear(CbusDePrueba.UnCbuValidoBloque1Str, CbusDePrueba.UnCbuValidoBloque2Str);
+            var cvu = builder.Crear(CvusDePrueba.UnCvuValidoBloque1Str, CvusDePrueba.UnCvuValidoBloque2Str);
+            Assert.IsType<Cbu>(cbu);
+            Assert.IsType<Cvu>(cvu);
+            Assert.Equal(CbusDePrueba.UnCbuValidoStr, cbu.ToString());
+            Assert.Equal(CvusDePrueba.UnCvuValidoStr, cvu.ToString());
+        }
+    }
+}
---
+++ Skiel85.Ar.Finanzas.Cxu/CxuBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Skiel85.Ar.Finanzas.Cxu
+{
+    public class CxuBuilder
+    {
+        public ICxu Crear(string cxuStr)
+        {
+            return cxuStr.StartsWith("000") ? (ICxu) new Cvu(cxuStr) : new Cbu(cxuStr);
+        }
+
+        public object Crear(string bloque1, string bloque2)
+        {
+            return bloque1.StartsWith("000") ? (ICxu) new Cvu(bloque1, bloque2) : new Cbu(bloque1, bloque2);
+        }
+    }
+}
